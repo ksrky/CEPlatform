097e19748e50c00c2d1e50228c0d68a06938b131
ksrky <xgrk9475@gmail.com>
2023-03-12 11:29:08 +0900

    update

--- python/control/PID.py
+++ python/control/PID.py
@@ -1,17 +1,22 @@
 import math
+import numpy as np
 
 
-def get_curvature(waypoints):
+def get_track_point(waypoints):
     mind = 10000
     theta = 0
+    pt1 = None
+    pt2 = None
     for j in range(len(waypoints)-1):
         (x1, y1) = waypoints[j]
         (x2, y2) = waypoints[j+1]
         d = (x1 * y2 - x2 * y1)**2 / ((x2 - x1)**2 + (y2 - y1)**2)
-        if d < mind:
+        if x1 * x1 + y1 * y1 < 10 and d < mind:  # TODO
             mind = d
             theta = math.acos((x2 - x1) / ((x2 - x1)**2 + (y2 - y1)**2)**.5)
-    return theta
+            pt1 = np.array(waypoints[j])
+            pt2 = np.array(waypoints[j+1])
+    return pt1, pt2
 
 
 class PID:
@@ -39,6 +44,11 @@ class PIDHandle(PID):
     def get_control(self, **kwargs):
         waypoints = kwargs['waypoints']
         heading = kwargs['heading']
-        theta = get_curvature(waypoints)
-        print(theta, heading)
-        return super().get_control(measurement=theta, dt=kwargs['dt'])
+
+        next = [(x, y) for (x, y) in waypoints if x > 0]
+        if not next:
+            y = 0
+        else:
+            (_, y) = next[0]
+
+        return super().get_control(measurement=-y, dt=kwargs['dt'])
